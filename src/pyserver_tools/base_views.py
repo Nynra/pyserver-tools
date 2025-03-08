29fb2262a06eb3233efea79a63106ff35612264a
Nynra <nynradev@pm.me>
2025-03-08 19:22:03 +0100

    Updated some templates

--- src/pyserver_tools/base_views.py
+++ src/pyserver_tools/base_views.py
@@ -1,3 +1,15 @@
+"""Base Views for Django.
+
+This module contains base views for Django that can be used to create views for
+basic database operations using default templates (not in the admin area).
+
+.. attention::
+
+    These views do not implement any autorization or authentication checks. It is
+    up to the developer to implement these checks in the views that inherit from
+    these base views.
+"""
+
 import logging
 from typing import Any
 
@@ -15,12 +27,18 @@ from django.views.generic import (
 )
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.forms import BaseModelForm
 
 
 logger = logging.getLogger(__name__)
 
 
-def error_page(request):
+def error_page(request: HttpRequest) -> HttpResponse:
+    """Return a custom error page.
+    
+    A simple error page that displays an error message and a link to the previous page.
+    """
     error_message = "An error occurred. Please try again later."
     previous_page_url = request.META.get("HTTP_REFERER", "/")
     context = {"error_message": error_message, "previous_page_url": previous_page_url}
@@ -30,7 +48,7 @@ def error_page(request):
     return render(request, "error_page.html", context)
 
 
-class BaseCreateView(CreateView):
+class PyserverBaseCreateView(CreateView):
     """Base view for creating a new model instance.
 
     This is a base class for creating a new model instance, it should be subclassed
@@ -42,18 +60,20 @@ class BaseCreateView(CreateView):
     - list_view_name: The name of the list view.
     """
 
-    template_name = "create_model.html"
-    context_object_name = "item"
+    template_name : str = "tools_templates/create_model.html"
+    model_name : str = None
+    detail_view_name : str = None
+    list_view_name : str = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if not hasattr(self, "template_name"):
+        if isinstance(self.template_name, type(None)):
             raise AttributeError("template_name must be set in the subclass")
-        if not hasattr(self, "model_name"):
+        if isinstance(self.model_name, type(None)):
             raise AttributeError("model_name must be set in the subclass")
-        if not hasattr(self, "detail_view_name"):
+        if isinstance(self.detail_view_name, type(None)):
             raise AttributeError("detail_view_name must be set in the subclass")
-        if not hasattr(self, "list_view_name"):
+        if isinstance(self.list_view_name, type(None)):
             raise AttributeError("list_view_name must be set in the subclass")
 
     def get_success_url(self) -> str:
@@ -64,10 +84,11 @@ class BaseCreateView(CreateView):
         context = super().get_context_data(**kwargs)
         context["model_name"] = self.model_name
         context["list_url"] = self.list_view_name
+        context["previous_page_url"] = self.request.META.get("HTTP_REFERER", "/")
         return context
 
 
-class BaseUpdateView(UpdateView):
+class PyserverBaseUpdateView(UpdateView):
     """Base view for updating an existing model instance.
 
     This is a base class for updating an existing model instance, it should be subclassed
@@ -79,18 +100,21 @@ class BaseUpdateView(UpdateView):
     - list_view_name: The name of the list view.
     """
 
-    template_name = "update_model.html"
-    context_object_name = "item"
+    template_name : str = "tools_templates/update_model.html"
+    context_object_name : str = None
+    model_name : str = None
+    detail_view_name : str = None
+    list_view_name : str = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if not hasattr(self, "template_name"):
+        if isinstance(self.template_name, type(None)):
             raise AttributeError("template_name must be set in the subclass")
-        if not hasattr(self, "model_name"):
+        if isinstance(self.model_name, type(None)):
             raise AttributeError("model_name must be set in the subclass")
-        if not hasattr(self, "detail_view_name"):
+        if isinstance(self.detail_view_name, type(None)):
             raise AttributeError("detail_view_name must be set in the subclass")
-        if not hasattr(self, "list_view_name"):
+        if isinstance(self.list_view_name, type(None)):
             raise AttributeError("list_view_name must be set in the subclass")
 
     def get_success_url(self) -> str:
@@ -106,10 +130,11 @@ class BaseUpdateView(UpdateView):
         context = super().get_context_data(**kwargs)
         context["model_name"] = self.model_name
         context["list_url"] = self.list_view_name
+        context["previous_page_url"] = self.request.META.get("HTTP_REFERER", "/")
         return context
 
 
-class BaseDeleteView(DeleteView):
+class PyserverBaseDeleteView(DeleteView):
     """Base view for deleting an existing model instance.
 
     This is a base class for deleting an existing model instance, it should be subclassed
@@ -120,16 +145,17 @@ class BaseDeleteView(DeleteView):
     - list_view_name: The name of the list view.
     """
 
-    template_name = "delete_model.html"
-    context_object_name = "item"
+    template_name : str = "tools_templates/delete_model.html"
+    model_name : str = None
+    list_view_name : str = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if not hasattr(self, "template_name"):
+        if isinstance(self.template_name, type(None)):
             raise AttributeError("template_name must be set in the subclass")
-        if not hasattr(self, "model_name"):
+        if isinstance(self.model_name, type(None)):
             raise AttributeError("model_name must be set in the subclass")
-        if not hasattr(self, "list_view_name"):
+        if isinstance(self.list_view_name, type(None)):
             raise AttributeError("list_view_name must be set in the subclass")
 
     def get_success_url(self) -> str:
@@ -153,10 +179,11 @@ class BaseDeleteView(DeleteView):
         context = super().get_context_data(**kwargs)
         context["model_name"] = self.model_name
         context["list_url"] = self.list_view_name
+        context["previous_page_url"] = self.request.META.get("HTTP_REFERER", "/")
         return context
 
 
-class BaseDetailView(DetailView):
+class PyserverBaseDetailView(DetailView):
     """Base view for showing an existing model instance.
 
     This is a base class for showing an existing model instance, it should be subclassed
@@ -170,22 +197,26 @@ class BaseDetailView(DetailView):
     - update_view_name: The name of the update view.
     """
 
-    template_name = "detail_model.html"
-    context_object_name = "item"
+    template_name : str = "tools_templates/detail_model.html"
+    detail_form : BaseModelForm = None
+    model_name : str = None
+    list_view_name : str = None
+    delete_view_name : str = None
+    update_view_name : str = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if not hasattr(self, "template_name"):
+        if isinstance(self.template_name, type(None)):
             raise AttributeError("template_name must be set in the subclass")
-        if not hasattr(self, "detail_form"):
+        if isinstance(self.detail_form, type(None)):
             raise AttributeError("detail_form must be set in the subclass")
-        if not hasattr(self, "model_name"):
+        if isinstance(self.model_name, type(None)):
             raise AttributeError("model_name must be set in the subclass")
-        if not hasattr(self, "list_view_name"):
+        if isinstance(self.list_view_name, type(None)):
             raise AttributeError("list_view_name must be set in the subclass")
-        if not hasattr(self, "delete_view_name"):
+        if isinstance(self.delete_view_name, type(None)):
             raise AttributeError("delete_view_name must be set in the subclass")
-        if not hasattr(self, "update_view_name"):
+        if isinstance(self.update_view_name, type(None)):
             raise AttributeError("update_view_name must be set in the subclass")
 
     def get_context_data(self, **kwargs):
@@ -193,58 +224,17 @@ class BaseDetailView(DetailView):
         context["form"] = self.detail_form(
             instance=self.object
         )  # Add the form to the context
+        context["item"] = self.object
         context["model_name"] = self.model_name
         context["list_url"] = self.list_view_name
         context["delete_url"] = self.delete_view_name
         context["update_url"] = self.update_view_name
+        context["previous_page_url"] = self.request.META.get("HTTP_REFERER", "/")
         return context
 
 
-class BaseRandomView(View):
-    """Base view for showing a random model instance.
 
-    This is a base class for showing a random model instance, it should be subclassed
-    and not used directly. The subclass should set the following attributes:
-
-    - template_name: The name of the template to render.
-    - model_name: The name of the model.
-    - random_view_name: The name of the random view.
-    """
-
-    template_name = "random_model.html"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if not hasattr(self, "template_name"):
-            raise AttributeError("template_name must be set in the subclass")
-        if not hasattr(self, "model_name"):
-            raise AttributeError("model_name must be set in the subclass")
-        if not hasattr(self, "random_view_name"):
-            raise AttributeError("random_view_name must be set in the subclass")
-
-    def get(self, request, *args, **kwargs):
-        model = self.model.get_random(user=self.request.user)
-        if model is None:
-            # Return no content available
-            return render(request, "no_content.html")
-
-        model.update_activation_date()
-        form = self.form_class(instance=model)
-        logger.info("User {} got a random {}".format(request.user, str(model)))
-        return render(request, self.template_name, self.get_context_data(form=form))
-
-    def get_context_data(self, **kwargs):
-        try:
-            context = super().get_context_data(**kwargs)
-        except AttributeError:
-            context = {}
-        context["form"] = kwargs["form"]
-        context["model_name"] = self.model_name
-        context["random_item_url"] = self.random_view_name
-        return context
-
-
-class BaseListView(ListView):
+class PyserverBaseListView(ListView):
     """Base view for showing a list of model instances.
 
     This is a base class for showing a list of model instances, it should be subclassed
@@ -258,48 +248,53 @@ class BaseListView(ListView):
     - list_view_name: The name of the list view.
     """
 
-    template_name = "list_models.html"
-    context_object_name = "items"
+    template_name : str = "tools_templates/list_models.html"
+    model_name : str = None
+    update_view_name : str = None
+    delete_view_name : str = None
+    create_view_name : str = None
+    list_view_name : str = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if not hasattr(self, "template_name"):
+        if isinstance(self.template_name, type(None)):
             raise AttributeError("template_name must be set in the subclass")
-        if not hasattr(self, "model_name"):
+        if isinstance(self.model_name, type(None)):
             raise AttributeError("model_name must be set in the subclass")
-        if not hasattr(self, "update_view_name"):
+        if isinstance(self.update_view_name, type(None)):
             raise AttributeError("update_view_name must be set in the subclass")
-        if not hasattr(self, "delete_view_name"):
+        if isinstance(self.delete_view_name, type(None)):
             raise AttributeError("delete_view_name must be set in the subclass")
-        if not hasattr(self, "create_view_name"):
+        if isinstance(self.create_view_name, type(None)):
             raise AttributeError("create_view_name must be set in the subclass")
-        if not hasattr(self, "list_view_name"):
+        if isinstance(self.list_view_name, type(None)):
             raise AttributeError("list_view_name must be set in the subclass")
 
-    def get_queryset(self) -> QuerySet[Any]:
-        return self.model.objects.filter(
-            user_config__user=self.request.user.id
-        ).order_by("-id")
-
     def get(self, request, *args, **kwargs):
-        # Get the page number from the request
-        page_number = request.GET.get("page")
-        queryset = self.get_queryset()  # Get the queryset
-        paginator = Paginator(queryset, 10)  # Create a paginator
-        page_obj = paginator.get_page(page_number)  # Get the page
-
         # Return the page
+        page_obj = self._get_pages(request)
         self.object_list = page_obj.object_list
         context = self.get_context_data()
-        context["page_obj"] = page_obj
+        context["items"] = page_obj
         return render(request, self.template_name, context)
+    
+    def _get_pages(self, request) -> QuerySet:
+        """Return the pages for the queryset."""
+        # Get the page number from the request
+        page_number = request.GET.get("page")
+        queryset = self.get_queryset()
+        paginator = Paginator(queryset, 10)
+        page_obj = paginator.get_page(page_number)
+        return page_obj
 
     # For the template to render correctly the model name variable and list url have to be passed
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        
         context["model_name"] = self.model_name
         context["update_url"] = self.update_view_name
         context["delete_url"] = self.delete_view_name
         context["create_url"] = self.create_view_name
         context["list_url"] = self.list_view_name
+        context["previous_page_url"] = self.request.META.get("HTTP_REFERER", "/")
         return context
